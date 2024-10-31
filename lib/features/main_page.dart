cad7cd35bbd256e27d7161a78ed628ef300eef5e
Ashley <ashleydenise.wvsu.edu.ph>
2024-10-31 20:53:23 +0800

    Fixed main.dart

--- lib/features/main_page.dart
+++ lib/features/main_page.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dynamic Button Grid',
+      title: 'AAC App',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
