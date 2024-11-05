fe6d26901123652e8fc3a8a2567a5798e7488e1f
Ashley <ashleydenise.wvsu.edu.ph>
2024-11-05 22:42:25 +0800

    Removed placeholder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cc206_aac_app/main.dart';
+import 'package:cc206_aac_app/features/main_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
